a4775592a8749fb6255aa18ddec329e651d581e6
Manuel <manuel.t.shade@gmail.com>
2024-10-17 14:14:30 +0100

    account page, update image

--- frontend/src/app/dashboard/account/page.tsx
+++ frontend/src/app/dashboard/account/page.tsx
@@ -1,6 +1,6 @@
 import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 import { ProfileForm } from "@/components/forms/ProfileForm";
-// import { ProfileImageForm } from "@/components/forms/ProfileImageForm";
+import { ProfileImageForm } from "@/components/forms/ProfileImageForm";
 
 export default async function AccountRoute() {
   const user = await getUserMeLoader();
@@ -11,7 +11,7 @@ export default async function AccountRoute() {
     <div className="grid grid-cols-1 lg:grid-cols-5 gap-4 p-4">
       Account Page
       <ProfileForm data={userData} className="col-span-3" />
-      {/* <ProfileImageForm data={userImage} className="col-span-2" /> */}
+      <ProfileImageForm data={userImage!} className="col-span-2" />
     </div>
   );
 }
